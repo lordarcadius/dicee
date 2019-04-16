af016c67a1e0b853c5d843133251660e8656952f
Vipul Jha <vipuljha08@gmail.com>
2019-04-16 14:43:21 +0530

    MainActivity: Add comments Just learning how to add comments properly
    Signed-off-by: Vipul Jha <vipuljha08@gmail.com>

--- app/src/main/java/in/vipul/dicee/MainActivity.java
+++ app/src/main/java/in/vipul/dicee/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.ImageView;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    //Variables declarations
     Button rolbut;
     ImageView diceleft, diceright;
     final int[] imageArray = {
@@ -26,21 +26,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Link layout elements to java
         rolbut = findViewById(R.id.roll_button);
         diceleft = findViewById(R.id.image_diceLeft);
         diceright = findViewById(R.id.image_diceRight);
 
+        //Add click on rolbut button
         rolbut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d("Dicee", "Working fine bruh");
+                //creating object that will generate random number
                 Random genRandomeNum = new Random();
+                //stores the randomly generated number in case of left dice
                 int number = genRandomeNum.nextInt(6);
                 Log.d("Dicee", "Random number is: "+number);
-
+                //Set image on left dice according to randomly generated number
                 diceleft.setImageResource(imageArray[number]);
-
+                //stores the randomly generated number in case of right dice
                 number = genRandomeNum.nextInt(6);
+                //Set image on right dice according to randomly generated number
                 diceright.setImageResource(imageArray[number]);
             }
         });
