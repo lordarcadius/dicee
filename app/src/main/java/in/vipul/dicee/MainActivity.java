4552e8b559db4b2178c5ae82e97647307754fb04
Vipul Jha <vipuljha08@gmail.com>
2019-04-16 14:27:07 +0530

    dicee: Add dice shake functionality
    It will give a randomly generated dice everytime user clicks the button.
    
    Signed-off-by: Vipul Jha <vipuljha08@gmail.com>

--- app/src/main/java/in/vipul/dicee/MainActivity.java
+++ app/src/main/java/in/vipul/dicee/MainActivity.java
@@ -2,12 +2,47 @@ package in.vipul.dicee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button rolbut;
+    ImageView diceleft, diceright;
+    final int[] imageArray = {
+            R.drawable.dice1,
+            R.drawable.dice2,
+            R.drawable.dice3,
+            R.drawable.dice4,
+            R.drawable.dice5,
+            R.drawable.dice6
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rolbut = findViewById(R.id.roll_button);
+        diceleft = findViewById(R.id.image_diceLeft);
+        diceright = findViewById(R.id.image_diceRight);
+
+        rolbut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Dicee", "Working fine bruh");
+                Random genRandomeNum = new Random();
+                int number = genRandomeNum.nextInt(6);
+                Log.d("Dicee", "Random number is: "+number);
+
+                diceleft.setImageResource(imageArray[number]);
+
+                number = genRandomeNum.nextInt(6);
+                diceright.setImageResource(imageArray[number]);
+            }
+        });
     }
 }
